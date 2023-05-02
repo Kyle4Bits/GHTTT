2046d544d9e3cbb96f9fbf67c55303c4b8365205
phatgg221 <s3929218@rmit.edu.vn>
2023-05-02 19:37:21 +0700

    Hello

--- src/main/java/com/example/ghttt/HelloApplication.java
+++ src/main/java/com/example/ghttt/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.ghttt;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -15,7 +16,8 @@ public class HelloApplication extends Application {
         Pane pane = new Pane();
         Label label = new Label("Hello");
         Label label1 = new Label("Hi");
-
+        Button clickme = new Button("Click me ");
+        pane.getChildren().add(clickme);
         pane.getChildren().addAll(label,label1);
         Scene scene = new Scene(pane, 320, 240);
         stage.setTitle("Hello!");
