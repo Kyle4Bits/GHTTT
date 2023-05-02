1a2a0bdf7fb430489b23c4be352df9229b64c908
Kyle4Bits <s3974876@rmit.edu.vn>
2023-05-02 19:28:08 +0700

    Hi label

--- src/main/java/com/example/ghttt/HelloApplication.java
+++ src/main/java/com/example/ghttt/HelloApplication.java
@@ -3,6 +3,8 @@ package com.example.ghttt;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +12,12 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Pane pane = new Pane();
+        Label label = new Label("Hello");
+        Label label1 = new Label("Hi");
+
+        pane.getChildren().addAll(label,label1);
+        Scene scene = new Scene(pane, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
